bbf4bfd5dc6c01c120a737e682223055e8bb7db7
Grrrotbag <gerard.keane@dfkeane.com>
2021-03-11 16:42:37 +0000

    initial commit

---
+++ gk-vue-components/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { GkVueComponentsSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  GkVueComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <gk-vue-components-sample />
+  </div>
+</template>
